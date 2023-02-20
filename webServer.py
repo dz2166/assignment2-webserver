9b6cbbebac219dd93561641885f67bd2bddfe221
dz2166 <125216580+dz2166@users.noreply.github.com>
2023-02-19 17:53:44 -0800

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -10,7 +10,6 @@ def webServer(port=13331):
     # Prepare a server socket
     serverSocket.bind(("", port))
 
-    # Listen for incoming connections
     serverSocket.listen(1)
 
     while True:
@@ -19,44 +18,23 @@ def webServer(port=13331):
         connectionSocket, addr = serverSocket.accept()
 
         try:
-            # Receive the client request message
             message = connectionSocket.recv(1024).decode()
-
-            # Extract the requested filename from the message
             filename = "helloworld.html"
+            f = open(filename, "rb")
+            outputdata = b"HTTP/1.1 200 OK\r\n"
+            outputdata += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
 
-            # Open the file and read its contents
-            with open(filename, "rb") as f:
-                file_data = f.read()
+            connectionSocket.send(outputdata)
 
-            # Build the HTTP response message
-            response_header = b"HTTP/1.1 200 OK\r\n"
-            response_header += b"Content-Type: text/html; charset=UTF-8\r\n"
-            response_header += b"Content-Length: " + str(len(file_data)).encode() + b"\r\n"
-            response_header += b"\r\n"
+            for i in f:
+                connectionSocket.send(i)
 
-            # Send the HTTP response message header to the client
-            connectionSocket.send(response_header)
-
-            # Send the file contents to the client
-            connectionSocket.send(file_data)
-
-            # Close the connection socket
+            f.close()
             connectionSocket.close()
 
-        except Exception as e:
-            # Send a 404 response message for invalid requests
-            error_message = b"<html><body><h1>404 Not Found</h1></body></html>"
-            response_header = b"HTTP/1.1 404 Not Found\r\n"
-            response_header += b"Content-Type: text/html; charset=UTF-8\r\n"
-            response_header += b"Content-Length: " + str(len(error_message)).encode() + b"\r\n"
-            response_header += b"\r\n"
-            response_data = response_header + error_message
-
-            # Send the HTTP response message to the client
-            connectionSocket.send(response_data)
-
-            # Close the connection socket
+        except FileNotFoundError:
+            outputdata = b"HTTP/1.1 404 Not Found\r\n\r\n"
+            connectionSocket.send(outputdata)
             connectionSocket.close()
 
     serverSocket.close()
