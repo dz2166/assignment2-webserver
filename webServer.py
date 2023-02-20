737de668be7579a499731410139c1b1a1b8521c4
dz2166 <125216580+dz2166@users.noreply.github.com>
2023-02-19 16:13:37 -0800

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -50,6 +50,9 @@ def webServer(port=13331):
             # Close client socket
             connectionSocket.close()
 
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        connectionSocket.send(response.encode())
+        
     serverSocket.close()
 
 
